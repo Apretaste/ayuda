3edd62c72c933956b72514c6b4fdad97533d714b
salvipascual <salvi.pascual@gmail.com>
2019-11-08 17:27:35 -0500

    move to v5

---
+++ scripts.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+	$('.modal').modal();
+	$('.tabs').tabs();
+});
+
+function sendMessage() {
+	var message = $('#message').val().trim();
+	if(message.length >= 30){
+		apretaste.send({
+			'command':'SOPORTE ESCRIBIR',
+			'data':{'message':message},
+			'redirect':false,
+			'callback':{'name':'sendMessageCallback','data':message}
+		});
+	}
+	else M.toast({html: 'Mínimo 30 caracteres'});
+}
+
+function messageLengthValidate() {
+	var message = $('#message').val().trim();
+	if(message.length <= 1000) $('.helper-text').html('Restante: '+(1000-message.length));
+	else $('.helper-text').html('Limite excedido');
+}
+
+function sendMessageCallback(message) {
+	// if first time, change headers
+	if($('#main .chat').length == 0) {
+		$('#main h1').remove();
+		$('#main p').remove();
+		$('#main').append('<h1>Su conversación con el soporte</h1>');
+		$('#main').append('<div class="chat"></div>');
+	};
+
+	// create bubble date
+	var now = new Date(Date.now()).toLocaleString();
+	now = now.replace('p. m.','pm');
+	now = now.replace('a. m.','am');
+
+	// append the bubble to teh screen
+	$('.chat').append('<div class="bubble me"><small><b>'+myUsername+'</b> - '+now+'<div class="chip small white-text blue lighten-1">NEW</div></small><br>'+message+'</div>');
+
+	// scroll to the last bubble
+	var lastBubble = $(".bubble:last-of-type");
+	if(lastBubble) {
+		$('html, body').animate({
+			scrollTop: $(".bubble:last-of-type").offset().top
+		}, 1000);
+	}
+}
+
+function vote(id, vote) {
+	// save the vote in the backend
+	apretaste.send({
+		"command": "AYUDA VOTAR",
+		"data": {'id':id,'vote':vote},
+		"redirect": false
+	});
+
+	// change the message
+	$('#survey').html('¡Gracias por dejarnos su opinión!');
+}
--- service.php
+++ service.php
@@ -1,19 +1,136 @@
 <?php
 
-class Ayuda extends Service
+class Service
 {
 	/**
-	 * Function executed when the service is called
+	 * Service starting point 
 	 *
 	 * @param Request
-	 * @return Response
-	 * */
-	public function _main(Request $request)
+	 * @param Response
+	 */
+	public function _main(Request $request, Response $response)
 	{
-		// create response
-		$response = new Response();
-		$response->subject = "Ayuda de Apretaste";
-		$response->createFromTemplate("basic.tpl", ["userEmail" => $request->email]);
-		return $response;
+		return $this->_faq($request, $response);
+	}
+
+	/**
+	 * Show the FAQ
+	 *
+	 * @param Request
+	 * @param Response
+	 */
+	public function _faq(Request $request, Response $response)
+	{
+		// get the faq
+		$faq = Connection::query("
+			SELECT id, title, views
+			FROM support_faq
+			ORDER BY (upvote - downvote) DESC");
+
+		// send data to the view
+		$response->setCache();
+		$response->setTemplate('faq.ejs', ['faq' => $faq]);
+	}
+
+	/**
+	 * Show the answer for an FAQ entry
+	 *
+	 * @param Request
+	 * @param Response
+	 */
+	public function _respuesta(Request $request, Response $response)
+	{
+		// get the ID
+		$id = $request->input->data->id;
+
+		// increase the views
+		Connection::query("UPDATE support_faq SET views=views+1 WHERE id=$id");
+
+		// get the faq
+		$answer = Connection::query("SELECT id, title, body, views FROM support_faq WHERE id=$id")[0];
+		$answer->body = nl2br($answer->body);
+
+		// send data to the view
+		$response->setCache();
+		$response->setTemplate('answer.ejs', ['answer' => $answer]);
+	}
+
+	/**
+	 * Vote on the answer
+	 *
+	 * @param Request
+	 * @param Response
+	 */
+	public function _votar(Request $request, Response $response)
+	{
+		// get the ID
+		$id = $request->input->data->id;
+		$vote = $request->input->data->vote;
+
+		// upvote or downvote
+		Connection::query("UPDATE support_faq SET {$vote}vote={$vote}vote+1 WHERE id=$id");
+	}
+
+	/**
+	 * Show the conversation with Support
+	 *
+	 * @param Request
+	 * @param Response
+	 */
+	public function _soporte(Request $request, Response $response)
+	{
+		// get the list of messages
+		$tickets = Connection::query("
+			SELECT A.*, B.username 
+			FROM support_tickets A 
+			JOIN person B
+			ON A.from_id = B.id
+			WHERE A.from_id = {$request->person->id} 
+			ORDER BY A.creation_date ASC");
+
+		// prepare chats for the view
+		$chat = [];
+		foreach ($tickets as $ticket) {
+			$message = new stdClass();
+			$message->class = $ticket->from_id == $request->person->id ? "me" : "you";
+			$message->from = $ticket->username;
+			$message->text = $ticket->body;
+			$message->date = date_format((new DateTime($ticket->creation_date)), 'd/m/Y h:i a');
+			$message->status = $ticket->status;
+			$chat[] = $message;
+		}
+
+		// send data to the view
+		$response->setTemplate('soporte.ejs', ['chat' => $chat]);
+	}
+
+	/**
+	 * Create a new ticket
+	 *
+	 * @param Request  $request
+	 * @param Response $response
+	 * @throws \Exception
+	 */
+	public function _escribir(Request $request, Response $response)
+	{
+		// get params to save
+		$email = $request->person->email;
+		$appName = $request->input->app ?? '';
+		$appVersion = $request->input->appversion ?? '';
+		$osVersion = $request->input->osversion ?? '';
+		$body = Connection::escape($request->input->data->message, 1024);
+
+		// insert the ticket
+		Connection::query("
+			INSERT INTO support_tickets (`from_id`, `subject`, `body`, app_name, app_version, os_version)
+			VALUES ({$request->person->id}, 'Ticket from $email', '$body', '$appName', '$appVersion', '$osVersion')");
+
+		// save report
+		Connection::query("
+			INSERT INTO support_reports (inserted, new_count) VALUES (CURRENT_DATE, 1)
+			ON DUPLICATE KEY UPDATE new_count=new_count+1");
+
+		// mark challenge as completed
+		Challenges::complete("write-to-support", $request->person->id);
 	}
 }
